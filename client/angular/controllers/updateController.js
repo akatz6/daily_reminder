0036242cb6dc95c87907fd2b93c968fe0f36aaed
aaronmk2 <aaronmk2@yahoo.com>
2016-07-29 08:32:50 -0700

    more validation and bootstrap

--- client/angular/controllers/updateController.js
+++ client/angular/controllers/updateController.js
@@ -12,8 +12,28 @@ app.controller('updateController', ['$scope', '$location', 'updateFactory', '$ro
 	$scope.submit = function(){
 		var object = {id:$scope.id, item:$scope.item, date:$scope.date}
 		updateFactory.edit(object , function(sendUser){
-			console.log(sendUser)
-			$location.url('/logged_in');
+			$scope.correct = false;
+			$scope.incorrect = false;
+			$scope.early = false;
+			$scope.to_small = false;
+			var date = new Date()
+			date.setHours(0,0,0,0)
+			if($scope.date < date){
+				$scope.early = true
+			} else if($scope.item.length < 5){
+				$scope.to_small = true
+			} 
+			else {
+				if(sendUser.data.success){
+					$scope.correct = true;
+					$scope.incorrect = false;
+					$location.url('/logged_in');
+				}else {
+					$scope.correct = false;
+					$scope.incorrect = true;
+				}
+			}
+			
 		})
 	}
 }]);
